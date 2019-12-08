e4ffe8b94dfa5e5080dbbfe658a1d8e2b34b231b
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-12-09 01:19:09 +0800

    update task1

--- task1/reducer.py
+++ task1/reducer.py
@@ -1,23 +1,23 @@
 import sys
 
-current_category = None
+current_label = None
 current_count = 0
-word = None
+label = None
 
 for line in sys.stdin:
     line = line.strip()
-    category, count = line.split('\t', 1)
+    label, count = line.split(" ", 1)
     try:
         count = int(count)
     except ValueError:
         continue
-    if current_category == category:
+    if current_label == label:
         current_count += count
     else:
-        if current_category:
-            print('%s\t%s' % (current_category, current_count))
+        if current_label:
+            print('%s\t%s' % (current_label, current_count))
         current_count = count
-        current_categoty = category
+        current_label = label
 
-if current_category == category:
-    print('%s\t%s' % (current_category, current_count))
+if current_label == label:
+    print('%s\t%s' % (current_label, current_count))
