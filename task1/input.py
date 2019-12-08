5d2834344c708f72f04c0693e2686bfad106b82a
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-12-08 11:47:21 +0800

    Update input.py

--- task1/input.py
+++ task1/input.py
@@ -3,9 +3,10 @@ from utils import tf, read_data
 
 data_path = sys.argv[1]
 dataset = read_data(data_path)
-for record in dataset:
-	example = tf.train.Example()
-	example.ParseFromString(record.numpy())
-	vid = example.features.feature['id'].bytes_list.value[0].decode("utf-8")
-	categories = example.features.feature['labels'].int64_list.value
-	print('#', str(categories).replace("[", "").replace("]","").replace(", ", "\t"))
+with open('test_data.txt', "wb") as f:
+	for record in dataset:
+		example = tf.train.Example()
+		example.ParseFromString(record.numpy())
+		vid = example.features.feature['id'].bytes_list.value[0].decode("utf-8")
+		categories = example.features.feature['labels'].int64_list.value
+		f.write(str(categories).replace("[", "").replace("]","").replace(", ", "\t"))
