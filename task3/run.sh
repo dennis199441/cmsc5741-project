c8ed3173b5596e64d9be3499bf5fa836b0c79ca8
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-12-10 13:40:01 +0800

    update task3 script

--- task3/run.sh
+++ task3/run.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 i=1
-while :
+while:
 do
-	hadoop jar ../../../../usr/lib/hadoop-mapreduce/hadoop-streaming.jar -file centroids.txt -file ./mapper.py -mapper ./mapper.py -file ./reducer.py -reducer ./reducer.py -input /testMapReduce/dataset -output /testMapReduce/mapreduce-output$i
+	yarn jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.1.3.jar \
+	-files hdfs:///yt8m-analysis/task3/centroids.txt,hdfs:///yt8m-analysis/task3/mapper.py,hdfs:///yt8m-analysis/task3/reducer.py \
+	-mapper 'python3 mapper.py' \
+	-reducer 'python3 reducer.py 10' \
+	-input /yt8m-analysis/task3/test_dataset.txt \
+	-output /yt8m-analysis/task3/output$i
+
 	rm -f centroids1.txt
-	hadoop fs -copyToLocal /testMapReduce/mapreduce-output$i/part-00000 centroids1.txt
+	hadoop fs -copyToLocal /yt8m-analysis/task3/output$i/part-00000 centroids1.txt
 	seeiftrue=`python reader.py`
 	if [ $seeiftrue = 1 ]
 	then
 		rm centroids.txt
-		hadoop fs -copyToLocal /testMapReduce/mapreduce-output$i/part-00000 centroids.txt
+		hadoop fs -copyToLocal /yt8m-analysis/task3/output$i/part-00000 centroids.txt
 		break
 	else
 		rm centroids.txt
-		hadoop fs -copyToLocal /testMapReduce/mapreduce-output$i/part-00000 centroids.txt
+		hadoop fs -copyToLocal /yt8m-analysis/task3/output$i/part-00000 centroids.txt
 	fi
 	i=$((i+1))
 done
