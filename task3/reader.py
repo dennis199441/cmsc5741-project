ab7c9f77cad5739f78fc015ba0e2478370e54cc8
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-12-10 14:54:18 +0800

    update reader.py

--- task3/reader.py
+++ task3/reader.py
@@ -2,17 +2,20 @@ from mapper import getCentroids
 
 #check if distance of centroids and centroids1 is less than 1
 def checkCentroidsDistance(centroids, centroids1):
-    f1x = abs(centroids[0][0] - centroids1[0][0])<1
-    f1y = abs(centroids[0][1] - centroids1[0][1])<1
-    f2x = abs(centroids[1][0] - centroids1[1][0])<1
-    f2y = abs(centroids[1][1] - centroids1[1][1])<1
-    f3x = abs(centroids[2][0] - centroids1[2][0])<1
-    f3y = abs(centroids[2][1] - centroids1[2][1])<1
-
-    if f1x and f1y and f2x and f2y and f3x and f3y:
-        print(1)
+    n1, n2 = len(centroids), len(centroids1)
+    n = None
+    if n1 < n2:
+        n = n1
     else:
-        print(0)
+        n = n2
+    flag = 1
+    while i < n:
+        for j in range(2):
+            if abs(centroids[i][j] - centroids1[i][j]) < 1:
+                flag = 0
+                break
+
+    print(flag)
 
 if __name__ == "__main__":
     centroids = getCentroids('centroids.txt')
