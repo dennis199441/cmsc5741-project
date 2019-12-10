3c684316fdaa2ed20ecc8cbbd7d35c9777a65e63
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-12-10 15:37:54 +0800

    debug task3

--- task3/reducer.py
+++ task3/reducer.py
@@ -31,7 +31,7 @@ def calculateNewCentroids():
             if count != 0:
                 # print the average of every cluster to get new centroids
                 # print(str(sum_x / count) + ", " + str(sum_y / count))
-                print(str(count) + ", " + str(count))
+                print(str(sum_x) + ", " + str(sum_y))
 
             current_centroid = centroid_index
             sum_x = x
@@ -41,7 +41,7 @@ def calculateNewCentroids():
     # print last cluster's centroids
     if current_centroid == centroid_index and count != 0:
         # print(str(sum_x / count) + ", " + str(sum_y / count))
-        print(str(count) + ", " + str(count))
+        print(str(sum_x) + ", " + str(sum_y))
 
 if __name__ == "__main__":
     calculateNewCentroids()
