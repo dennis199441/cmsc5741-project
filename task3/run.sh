1d534e10d0c8f48e437349771c25660e3927e371
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-12-10 13:44:22 +0800

    fixed script

--- task3/run.sh
+++ task3/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 i=1
-while:
+while :
 do
 	yarn jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.1.3.jar \
 	-files hdfs:///yt8m-analysis/task3/centroids.txt,hdfs:///yt8m-analysis/task3/mapper.py,hdfs:///yt8m-analysis/task3/reducer.py \
@@ -11,7 +11,7 @@ do
 
 	rm -f centroids1.txt
 	hadoop fs -copyToLocal /yt8m-analysis/task3/output$i/part-00000 centroids1.txt
-	seeiftrue=`python reader.py`
+	seeiftrue=`python3 reader.py`
 	if [ $seeiftrue = 1 ]
 	then
 		rm centroids.txt
