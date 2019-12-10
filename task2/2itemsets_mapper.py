e7a4b31834337d0096f0d65e6074439e58fa4f0d
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-12-10 10:23:02 +0800

    task2 fixed indentation problem

--- task2/2itemsets_mapper.py
+++ task2/2itemsets_mapper.py
@@ -1,9 +1,9 @@
 import sys, re
 
 for line in sys.stdin: 
-   	line = line.strip()
-   	words = re.findall(r"[\'A-Za-z0-9]+", line)
-   	# for each word in list pair it with the others.
-	for i in range(0,len(words)): 
+	line = line.strip()
+	words = re.findall(r"[\'A-Za-z0-9]+", line)
+	# for each word in list pair it with the others.
+	for i in range(0,len(words)):
 		for j in range(i+1,len(words)):
 			print('%s\t%s\t%s' % (words[i],words[j],1))	
