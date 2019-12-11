25d2b01f204aa04b8408558059305a38759272cc
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-12-11 10:11:12 +0800

    uniformly generate centroids

--- task3/gen_centroids.py
+++ task3/gen_centroids.py
@@ -1,13 +1,14 @@
 import sys
-from random import random
+from random import uniform
 
 clusters = int(sys.argv[1]) # number of cluster
 dimensions = int(sys.argv[2]) # number of dimensions
+bound = int(sys.argv[3]) # random number bound
 
 with open("centroids.txt", "w") as f:
 	for i in range(clusters):
 		for d in range(dimensions - 1):
-			f.write(str(random()) + ", ")
-		f.write(str(random()) + "\n")
+			f.write(str(uniform(-bound, bound)) + ", ")
+		f.write(str(uniform(-bound, bound)) + "\n")
 
 print("Generated centroids.txt")
