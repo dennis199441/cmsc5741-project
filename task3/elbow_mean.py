e361dddfbf2c5bbe10b4203d37a2b22aa7c792a2
dennis199441 <wcgnuehc@yahoo.com.hk>
2019-12-12 10:38:44 +0800

    Update elbow_mean.py

--- task3/elbow_mean.py
+++ task3/elbow_mean.py
@@ -1,4 +1,5 @@
 import os, sys
+import matplotlib.pyplot as plt
 
 path = sys.argv[1] # elbow results
 k = int(sys.argv[2]) # k value
@@ -6,7 +7,6 @@ result = []
 summ = 0
 i = 1
 while i <= k:
-	print("i = {}".format(i))
 	with open(path + "{}_elbow.txt".format(i), "r") as f:
 		line = f.readline()
 		count = 0
@@ -17,7 +17,6 @@ while i <= k:
 					line = line.strip()
 					line = line.split()
 					summ += float(line[1])
-					print(line)
 				except:
 					break
 			line = f.readline()
@@ -26,4 +25,8 @@ while i <= k:
 	summ = 0
 	i += 1
 
-print(result)
+plt.plot(result)
+plt.title("Elbow curve")
+plt.ylabel("Average cluster squared distance")
+plt.xlabel("Number of cluster k")
+plt.show()
